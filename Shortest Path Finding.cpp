a8e5d15f272041b175a5298182f204be6af6015c
MD. Jahid Hasan Jim <152358542+jim2107054@users.noreply.github.com>
2024-04-23 21:57:30 +0600

    Shortest Path Finding.cpp

--- Shortest Path Finding.cpp
+++ Shortest Path Finding.cpp
@@ -1,55 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void addEdge(vector<vector<int>>&adjList, int u, int v){
-  adjList[u].push_back(v);
-  adjList[v].push_back(u);
+void addEdge(vector<vector<int>>& adjList, int u, int v) {
+    adjList[u].push_back(v);
+    adjList[v].push_back(u);
 }
 
-//the shortest path.
-vector<int> shortestPath(vector<vector<int>>&adjList,vector<bool>&visited,int start, int end){
-   vector<int>parent;
-   queue<int>q;
-   visited[start]=true;
-   q.push(start);
-   parent[start]=-1;
+// The shortest path.
+vector<int> shortestPath(vector<vector<int>>& adjList, vector<bool>& visited, int start, int end) {
+    vector<int> parent(adjList.size(), -1);
+    queue<int> q;
+    visited[start] = true;
+    q.push(start);
+    parent[start] = -1;
 
-   while(!q.empty()){
-    int frontNode = q.front();
-    q.pop();
+    while (!q.empty()) {
+        int frontNode = q.front();
+        q.pop();
 
-    for(auto neighbor:adjList[frontNode]){
-        if(!visited[neighbor]){
-            visited[neighbor]=true;
-            parent[neighbor]=frontNode;
-            q.push(neighbor);
+        for (auto neighbor : adjList[frontNode]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                parent[neighbor] = frontNode;
+                q.push(neighbor);
+            }
         }
-     }
-   }
-
-   //prepare shortest path.
-   vector<int>ans;
-   int currentNode = end;
-   ans.push_back(currentNode);
-   while(currentNode != start){
-    currentNode = parent[currentNode];
-    ans.push_back(currentNode);
-   }
-
-   reverse(ans.begin(),ans.end());
-   return ans;
-}
-
-int main()
-{
-    int v=5;
-    int e=6;
-    vector<vector<int>>adjList;
-    int a,b;
-    for(int i=0;i<e;i++){
-        cin>>a>>b;
-        addEdge(adjList,a,b);
     }
 
+    // Prepare shortest path.
+    vector<int> ans;
+    int currentNode = end;
+    ans.push_back(currentNode);
+    while (currentNode != start) {
+        currentNode = parent[currentNode];
+        ans.push_back(currentNode);
+    }
 
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main() {
+    int v = 5;
+    int e = 6;
+    vector<vector<int>> adjList(v); 
+    vector<bool> visited(v, false); 
+    int a, b;
+    for (int i = 0; i < e; i++) {
+        cin >> a >> b;
+        addEdge(adjList, a, b);
+    }
+    vector<int> path = shortestPath(adjList, visited, 0, 4);
+    cout << "Shortest path from 0 to 4: ";
+    for (int node : path) {
+        cout << node << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
